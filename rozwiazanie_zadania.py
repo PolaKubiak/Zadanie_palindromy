1715293fa86df90a65dfdf96be76dab53dfcf19c
Paulina Kubiak <pola.kubiak@gmail.com>
2024-02-12 21:06:49 +0100

    add final code

--- rozwiazanie_zadania.py
+++ rozwiazanie_zadania.py
@@ -1,1 +1,17 @@
-print("działa?")
+#Define a function 
+
+def isPalindrome(string): 
+
+    if (string == string[::-1]): 
+
+        return "True. The string is a palindrome." 
+
+    else: 
+
+        return "False. The string is not a palindrome." 
+
+#Enter input string 
+
+string=input("Enter string:")
+
+print(isPalindrome(string))
